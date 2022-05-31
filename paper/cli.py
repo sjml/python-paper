1158c3fd3a5a5f1e457b8f10ba929682ecc2c54e
Shane Liesegang <shane@techie.net>
2022-05-31 22:42:10 +0300

    ok if I'm writing help text we're prolly done

--- paper/cli.py
+++ paper/cli.py
@@ -3,7 +3,10 @@ import typer
 from .formats import Format
 from .shared import PAPER_STATE
 
-_app = typer.Typer()
+_app = typer.Typer(
+    add_completion=False,
+    no_args_is_help=True,
+)
 
 
 def main():
@@ -12,14 +15,18 @@ def main():
 
 @_app.callback()
 def paper(verbose: bool = False):
-    """
-    Shane's little paper-writing utility.
+    """\b
+    Shane’s little paper-{writing|managing|building} utility
+        <https://github.com/sjml/paper>
     """
     PAPER_STATE["verbose"] = verbose
 
 
 @_app.command()
 def new(project_name: str):
+    """
+    Create the scaffolding for a new writing/research project.
+    """
     from .project_setup import new
 
     new(project_name)
@@ -27,20 +34,22 @@ def new(project_name: str):
 
 @_app.command()
 def init():
+    """
+    While in an empty directory, set it up for a project.
+    (Called as part of the process for `new`.)
+    """
     from .project_setup import init
 
     init()
 
 
 @_app.command()
-def reinit():
-    from .project_setup import reinit
-
-    reinit()
-
-
-@_app.command()
 def dev():
+    """
+    Set up a project for development work on paper itself.
+    Deletes the local `.paper_resources` directory and symlinks
+    the template’s version, so changes here affect the actual program.
+    """
     from .project_setup import dev
 
     dev()
@@ -48,6 +57,9 @@ def dev():
 
 @_app.command()
 def build(output_format: Format = typer.Option(None)):
+    """
+    Generate versions of the paper ready for submission.
+    """
     from .build import build
 
     build(output_format)
@@ -55,6 +67,9 @@ def build(output_format: Format = typer.Option(None)):
 
 @_app.command()
 def fmt(wrap: bool = True, columns: int = 80):
+    """
+    Run an automated formatter on all the local Markdown files.
+    """
     from .mdfmt import fmt
 
     fmt(wrap, columns)
@@ -62,6 +77,9 @@ def fmt(wrap: bool = True, columns: int = 80):
 
 @_app.command()
 def wc():
+    """
+    Print word count metrics for the project.
+    """
     from .wc import wc
 
     wc()
@@ -69,6 +87,9 @@ def wc():
 
 @_app.command()
 def save(message: str = typer.Option(..., prompt="Commit message?")):
+    """
+    Make a git commit with some extra tracking data.
+    """
     from .save import save
 
     save(message)
@@ -76,6 +97,9 @@ def save(message: str = typer.Option(..., prompt="Commit message?")):
 
 @_app.command()
 def push():
+    """
+    Push local git changes to the remote repository.
+    """
     from .save import push
 
     push()
@@ -83,6 +107,9 @@ def push():
 
 @_app.command()
 def web():
+    """
+    Open the remote repository’s GitHub site.
+    """
     from .save import web
 
     web()
